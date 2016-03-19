1a8d0a0792409295b393e7499b813cfc4c923784
PatMulvihill <mulvihillp993@yahoo.com>
2016-03-19 05:02:38 +0000

    finished exercise 10

--- jugglingasync.js
+++ jugglingasync.js
@@ -30,38 +30,60 @@
 
 const http = require('http');
 const bl = require('bl');
-const url1 = process.argv[2];
-const url2 = process.argv[3];
-const url3 = process.argv[4];
 var results = [];
+var count = 0;
 
-http.get(url1, function(response) {
-    response.pipe(bl(function(err, data) {
-        if (err)
-            return console.error(err);
-        data = data.toString();
-        results[0] = data;
-    }));
-});
+function printResults() {
+    for (var i = 0; i < 3; i++) {
+        console.log(results[i]);
+    }
+}
 
-http.get(url2, function(response) {
-    response.pipe(bl(function(err, data) {
-        if (err)
-            return console.error(err);
-        data = data.toString();
-        results[1] = data;
-    }));
-});
-
-http.get(url3, function(response) {
-    response.pipe(bl(function(err, data) {
-        if (err)
-            return console.error(err);
-        data = data.toString();
-        results[2] = data;
-    }));
-});
+function httpGet(index) {
+    http.get(process.argv[2 + index], function(response) {
+        response.pipe(bl(function(err, data) {
+            if (err)
+                return console.error(err);
+            results[index] = data.toString();
+            count ++;
+            
+            if (count == 3) {
+                printResults();
+            }
+        }));
+    });
+}
 
 for (var i = 0; i < 3; i++) {
-    console.log(results[i]);
+    httpGet(i);
 }
+
+// learnyounode's suggested solution:
+
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
+
+// function printResults() {
+//     for (var i = 0; i < 3; i++)
+//         console.log(results[i])
+// }
+
+// function httpGet(index) {
+//     http.get(process.argv[2 + index], function(response) {
+//         response.pipe(bl(function(err, data) {
+//             if (err)
+//                 return console.error(err)
+
+//             results[index] = data.toString()
+//             count++
+
+//             if (count == 3)
+//                 printResults()
+//         }))
+//     })
+// }
+
+// for (var i = 0; i < 3; i++)
+//     httpGet(i)
